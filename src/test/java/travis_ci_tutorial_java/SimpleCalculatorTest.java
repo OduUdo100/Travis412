ac808790adce1c40256481e2edb5bf52c7aacd2e
student <student@odus-mbp.fios-router.home>
2019-11-25 22:09:47 -0500

    Still learning it.

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -10,5 +10,8 @@ public class SimpleCalculatorTest {
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
+		assertEquals(calc.sub(5, 3), 2);
+		assertEquals(calc.mult(5, 2), 10);
+		assertEquals(calc.div(10,5),2);
 	}
 }
